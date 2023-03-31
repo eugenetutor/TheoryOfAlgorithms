7d806e8385f50e39663e4c475369ca77497de463
demenkoeugene <mimosa.01-purgers@icloud.com>
2023-03-31 20:21:45 +0300

    lesson3103

---
+++ src/Lesson2.java
@@ -0,0 +1,37 @@
+public class Lesson2 {
+
+    public static void main(String[] args) {
+        boolean result = isPrime(97);
+        System.out.println("результат " + result);
+    }
+
+        public static boolean isPrime(int n){
+            if (n <= 1) {
+                return false;
+            }
+
+            if (n == 2 || n == 3) {
+                return true;
+            }
+
+            if (n % 2 == 0 || n % 3 == 0) {
+                return false;
+            }
+
+            for (int i = 5; i <= Math.sqrt(n); i += 2) {
+                if (n % i == 0) {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+
+    }
+
+
+
+
+
+
