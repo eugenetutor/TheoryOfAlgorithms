f545180f5689b3c9de32e34562bd5947f4b1ac4c
demenkoeugene <mimosa.01-purgers@icloud.com>
2023-04-01 22:01:50 +0300

    HomeWork #2

--- src/Lesson2.java
+++ src/Lesson2.java
@@ -6,41 +6,44 @@ public class Lesson2 {
     }
 
 
-    public static boolean isPrime(int n) {
-        if (n <= 1) {
-            return false;
-        }
+//    public static boolean isPrime(int n) {
+//        if (n <= 1) {
+//            return false;
+//        }
+//
+//        for (int i = 2; i <= Math.sqrt(n); i++) {
+//            System.out.println(i);
+//            System.out.println(Math.sqrt(n));
+//            if (n % i == 0) {
+//                return false;
+//            }
+//        }
+//
+//        return true;
+//    }
 
-        for (int i = 2; i <= Math.sqrt(n); i++) {
-            if (n % i == 0) {
+        public static boolean isPrime(int n){
+            if (n <= 1) {
                 return false;
             }
+
+            if (n == 2 || n == 3) {
+                return true;
+            }
+
+            if (n % 2 == 0 || n % 3 == 0) {
+                return false;
+            }
+
+            for (int i = 5; i <= Math.sqrt(n); i += 2) {
+                System.out.println("i="+i);
+                if (n % i == 0) {
+                    return false;
+                }
+            }
+
+            return true;
         }
-
-        return true;
-    }
-
-//        public static boolean isPrime(int n){
-//            if (n <= 1) {
-//                return false;
-//            }
-//
-//            if (n == 2 || n == 3) {
-//                return true;
-//            }
-//
-//            if (n % 2 == 0 || n % 3 == 0) {
-//                return false;
-//            }
-//
-//            for (int i = 5; i <= Math.sqrt(n); i += 2) {
-//                if (n % i == 0) {
-//                    return false;
-//                }
-//            }
-//
-//            return true;
-//        }
 
 
     }
