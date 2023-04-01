7984d5f8000620cb921572e0f9671e894e111e3d
demenkoeugene <mimosa.01-purgers@icloud.com>
2023-04-01 16:22:50 +0300

    commit0104

--- src/Lesson2.java
+++ src/Lesson2.java
@@ -5,27 +5,42 @@ public class Lesson2 {
         System.out.println("результат " + result);
     }
 
-        public static boolean isPrime(int n){
-            if (n <= 1) {
-                return false;
-            }
 
-            if (n == 2 || n == 3) {
-                return true;
-            }
-
-            if (n % 2 == 0 || n % 3 == 0) {
-                return false;
-            }
-
-            for (int i = 5; i <= Math.sqrt(n); i += 2) {
-                if (n % i == 0) {
-                    return false;
-                }
-            }
-
-            return true;
+    public static boolean isPrime(int n) {
+        if (n <= 1) {
+            return false;
         }
+
+        for (int i = 2; i <= Math.sqrt(n); i++) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+//        public static boolean isPrime(int n){
+//            if (n <= 1) {
+//                return false;
+//            }
+//
+//            if (n == 2 || n == 3) {
+//                return true;
+//            }
+//
+//            if (n % 2 == 0 || n % 3 == 0) {
+//                return false;
+//            }
+//
+//            for (int i = 5; i <= Math.sqrt(n); i += 2) {
+//                if (n % i == 0) {
+//                    return false;
+//                }
+//            }
+//
+//            return true;
+//        }
 
 
     }
